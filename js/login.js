3cd0e09d41de91e34bf4cd1dff2c1cfe1e90a7b3
sbss07 <sebasfr01@gmail.com>
2021-08-19 10:15:47 -0300

    Entrega 1
    Productos y login

--- js/login.js
+++ js/login.js
@@ -1,6 +1,17 @@
+const formLogin = document.getElementById("login");
+
+formLogin.addEventListener("submit", function (event) {
+	// stop form submission
+	event.preventDefault();
+
+    sessionStorage.setItem("cargado", "cargado");
+    window.location.replace("index.html");
+	
+});
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
 
 });
--- js/products.js
+++ js/products.js
@@ -1,6 +1,47 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+const showList = (autos) => {
+  const list = document.getElementById('products');
+  console.log(list);
+  
+  list.setAttribute('class', "list");
+  
+  for (let auto of autos) {
+    const item = document.createElement("div");
+    const img = document.createElement("img");
+    const name = document.createElement("h1");
+    const description = document.createElement('p');
+    var price = document.createElement('p');
+    var priceText = document.createTextNode(`${auto.currency} ${auto.cost}`)
+    var soldCount = document.createElement('p');
+    var soldCountText = document.createTextNode(`${auto.soldCount} vendidos`)
+    soldCount.appendChild(soldCountText);
+    price.appendChild(priceText)
 
+    name.innerText = auto.name;
+    description.innerText = auto.description;
+     
+    img.setAttribute('src', auto.imgSrc);
+    img.setAttribute('class', 'img');
+
+    name.setAttribute('class', 'name');
+    description.setAttribute('class', 'description');
+    price.setAttribute('class', 'price');
+    soldCount.setAttribute('class', 'soldCount');
+    item.setAttribute('class', 'item');
+
+    item.appendChild(img);
+    item.appendChild(name);
+    item.appendChild(description);
+    item.appendChild(price);
+    item.appendChild(soldCount);
+    list.appendChild(item);
+
+    console.log(auto);
+  }
+  document.getElementById('products').appendChild(lsit)
+  document.body.appendChild(list); // Se muestra en pantalla la lista total
+};
+
+document.addEventListener("DOMContentLoaded", async function (e) {
+  const autos = (await getJSONData(PRODUCTS_URL)).data;
+  showList(autos);
 });
