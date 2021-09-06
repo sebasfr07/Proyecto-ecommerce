e54b28be33d0c6b8bda474daec18c60977f6cf18
sbss07 <sebasfr01@gmail.com>
2021-09-05 21:02:09 -0300

    Entrega 3 solucion inicial

--- js/product-info.js
+++ js/product-info.js
@@ -2,26 +2,34 @@
 
 
 
-
-function showProduct(array) {
+function showProduct(product) {
     let producto = "";
-    array.forEach(info => {
-        producto = + `
-            <p>${info.name}</p>
+    //JSON.parse(array);
+    for (let info of Object.keys(product)) {
+        
+        var inform = product[info]
+        console.log(info, inform);
+
+        producto += `
+            <p>${inform}</p>
         `
-        console.log(producto)
-    });
+    }
 
     document.getElementById("infoProductoDiv").innerHTML = producto;
 }
 
-
-
+//JSON.parse()
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", async function () {
-    const array = (await getJSONData(PRODUCTS_URL)).data;
-    showProduct(array);
+document.addEventListener("DOMContentLoaded", async function (e) {
+    const product = (await getJSONData(PRODUCT_INFO_URL)).data;
+    let stringProduct = JSON.stringify(product);
+    const arrayProduct = JSON.parse(stringProduct);
+    //console.log(JSON.stringify(array));
+    console.log(product);
+    console.log(arrayProduct);
+    showProduct(arrayProduct);
+
 });
