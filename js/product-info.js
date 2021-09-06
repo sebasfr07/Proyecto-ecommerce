5e5ad36d6a5e364c7d1e7fe69f2897370023efbe
sbss07 <sebasfr01@gmail.com>
2021-09-05 22:14:09 -0300

    Maciel

--- js/product-info.js
+++ js/product-info.js
@@ -1,22 +1,52 @@
 
+const showImages = (product) => {
+    let imagenes = ""
+    for (let images of product.images) {
+        imagenes += `
+            <img src="${images}" class="imagenesAuto"></img>
+        `
+    };
+    document.getElementById("infoProductoDiv").innerHTML = imagenes;
+};
 
 
+const showProductInfo = (product) => {
+    let infoProductos = "";
+    console.log(product);
+    showImages(product);
 
-function showProduct(product) {
-    let producto = "";
-    //JSON.parse(array);
-    for (let info of Object.keys(product)) {
-        
-        var inform = product[info]
-        console.log(info, inform);
-
-        producto += `
-            <p>${inform}</p>
+    for (const [key, value] of Object.keys(product)) {
+        console.log(value);
+        infoProductos += `
+            <p>${value}</p>
         `
     }
-
-    document.getElementById("infoProductoDiv").innerHTML = producto;
+    document.getElementById("nameProduct").innerHTML = infoProductos;
 }
+
+//https://dev.to/duxtech/5-maneras-de-iterar-un-objeto-en-javascript-jkn
+
+// function showProduct(product) {
+//     let producto = "";
+//     //JSON.parse(array);
+//     //console.log(product.images)
+
+
+
+//     for (let info of Object.keys(product)) {
+
+//         var inform = product[info]
+//         //console.log(inform);
+
+//         producto += `
+            
+//         `
+//     }
+
+//     document.getElementById("infoProductoDiv").innerHTML = producto;
+// }
+
+
 
 //JSON.parse()
 
@@ -25,11 +55,11 @@ function showProduct(product) {
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", async function (e) {
     const product = (await getJSONData(PRODUCT_INFO_URL)).data;
-    let stringProduct = JSON.stringify(product);
-    const arrayProduct = JSON.parse(stringProduct);
+    //let stringProduct = JSON.stringify(product);
+    //const arrayProduct = JSON.parse(stringProduct);
     //console.log(JSON.stringify(array));
-    console.log(product);
-    console.log(arrayProduct);
-    showProduct(arrayProduct);
-
+    //console.log(arrayProduct);
+    //showProduct(arrayProduct);
+    //showImages(product);
+    showProductInfo(product);
 });
