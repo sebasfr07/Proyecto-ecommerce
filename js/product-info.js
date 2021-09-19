d5a26ea298bdbd85a9a146f881b69fdfd1aec297
sbss07 <sebasfr01@gmail.com>
2021-09-19 15:10:34 -0300

    Entrega 3(Completa)

--- js/product-info.js
+++ js/product-info.js
@@ -1,69 +1,70 @@
 const formComentarios = document.getElementById("formComment");
 
-
 const showImages = (product) => {
-    let imagenes = ""
-    for (let images of product.images) {
-        imagenes += `
+  let imagenes = "";
+  for (let images of product.images) {
+    imagenes += `
             <img src="${images}" class="imagenesAuto"></img>
-        `
-    };
-    document.getElementById("imgProductoDiv").innerHTML = imagenes;
+        `;
+  }
+  document.getElementById("imgsProduct").innerHTML = imagenes;
 };
 
-
 const showProductInfo = (product) => {
-    let infoProductos = "";
-    console.log(product.name);
-    showImages(product);
-    let myProductArray = Object.entries(product)
-    console.log(myProductArray[1][1])
-    
+  // let infoProductos = "<ul>";
+  let infoProductos = "";
+  showImages(product);
+  //titulo
+  infoProductos += `<div><h1 class='onyxTitle'>${product.name}</h1></div>`; 
+  //categoria
+  infoProductos += `<div><h5>Categoria: <a class='onyxCategory' href='category-info.html'>${product.category}</a></h5></div>`;
+  //precio
+  infoProductos += `<div><h5 class='onyxCost'><span>${product.cost} ${product.currency}</span></h5></div>`;
+  //vendidos
+  infoProductos += `<div class='onyxSoldCount'><p>${product.soldCount} unidades vendidas</p></div>`;
+  //descripcion
+  infoProductos += `<div><p class='onyxDescription'>${product.description}</p></div>`;
+  
+  console.log(product);
+  document.getElementById("infoProduct").innerHTML = infoProductos;
+};
 
-    document.getElementById("nameProduct").innerHTML = infoProductos;
-}
+formComentarios.addEventListener("submit", function (e) {
+  e.preventDefault();
+  document.getElementById("comentario").value = "";
+});
 
-formComentarios.addEventListener("submit", function(e) {
-    e.preventDefault();
-    document.getElementById("comentario").value = "";
-})
+const showComments = (comments) => {
+  let infoComments = "<div class='contendor__comentarios'>";
 
-//https://dev.to/duxtech/5-maneras-de-iterar-un-objeto-en-javascript-jkn
-
-// function showProduct(product) {
-//     let producto = "";
-//     //JSON.parse(array);
-//     //console.log(product.images)
-
-
-
-//     for (let info of Object.keys(product)) {
-
-//         var inform = product[info]
-//         //console.log(inform);
-
-//         producto += `
-            
-//         `
-//     }
-
-//     document.getElementById("infoProductoDiv").innerHTML = producto;
-// }
-
-
-
-//JSON.parse()
+  comments.forEach((comment) => {
+    let stars = "";
+    for (let i = 0; i < comment.score; i++) {
+      stars += "★";
+    }
+    infoComments += `
+    <div class='comment'>
+        <div class='estrellitasDiv'>
+          <span class='estrellitas'>${stars}</span>
+          <b class='hora'>${comment.dateTime.slice(11, 19)}</b>
+        </div>
+      <h6>
+      <b class='fecha'>${comment.dateTime.slice(0, 10)}:</b> ${
+      comment.user
+    }</h6>
+      <p>${comment.description}</p>
+    </div> 
+    `;
+  });
+  infoComments += "</div>";
+  document.getElementById("infoComments").innerHTML = infoComments;
+};
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", async function (e) {
-    const product = (await getJSONData(PRODUCT_INFO_URL)).data;
-    //let stringProduct = JSON.stringify(product);
-    //const arrayProduct = JSON.parse(stringProduct);
-    //console.log(JSON.stringify(array));
-    //console.log(arrayProduct);
-    //showProduct(arrayProduct);
-    //showImages(product);
-    showProductInfo(product);
+  const product = (await getJSONData(PRODUCT_INFO_URL)).data;
+  showProductInfo(product);
+  showComments((await getJSONData(PRODUCT_INFO_COMMENTS_URL)).data);
 });
