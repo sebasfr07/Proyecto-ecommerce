fe7c8a66a6e0b66894af36542222f4bbfe73fda9
sbss07 <sebasfr01@gmail.com>
2021-09-30 11:02:15 -0300

    entrega4

--- js/product-info.js
+++ js/product-info.js
@@ -84,6 +84,7 @@ const showRelated = (product, products) => {
   document.getElementById('relatedProductsDiv').innerHTML = relProducts;
 }
 
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
