f90951aa65dd1c01352662f5f59fb358e3314649
sbss07 <sebasfr01@gmail.com>
2021-10-16 16:55:04 -0300

    faltaMetodoDePago

--- js/cart.js
+++ js/cart.js
@@ -6,6 +6,8 @@ function showCart(cart) {
   let article = cart.articles[0];
   var quantity = article.count;
 
+  //Hace una lista de los radio buttons, los recorre y devuelve el valor del seleccionado.
+
   let cartInfo = "";
   cartInfo += `
     <div class="Cart-Items">
@@ -31,9 +33,16 @@ function showCart(cart) {
  <div class="Subtotal">Sub-Total</div>
  <div class="items" id="itemQuantity">${quantity} item</div>
  </div>
- <div class="total-amount" id="subTotal">${article.unitCost * quantity}$</div>
+ <div class="subtotal-amount" id="subTotal">${
+   article.unitCost * quantity
+ }$</div>
  </div>
- <button class="button">Checkout</button>
+ <br>
+ <div id="totalDiv">
+ <div class="totalText">Total</div>
+ <div class="total-amount" id="Total">${quantity*article.unitCost + (quantity*article.unitCost*15)/100}$</div>
+ </div>
+ <input type="submit" class="button" value="Checkout">
  </div>
     `;
   document.getElementById("cartProducts").innerHTML = cartInfo;
@@ -42,6 +51,7 @@ function showCart(cart) {
   let unitCost = article.unitCost;
 
   countProduct(quantity, subTotal, unitCost, itemQuantity);
+  showTotalShipping(quantity, unitCost);
 }
 
 function countProduct(quantity, subtotal, unitCost, itemQuantity) {
@@ -50,24 +60,53 @@ function countProduct(quantity, subtotal, unitCost, itemQuantity) {
   let newQuantity = document.getElementById("quantity");
   let subTotal = subtotal;
   let item = itemQuantity;
+  let sTotal = quantity * unitCost;
+
+  let shippingValue = 15;
+  let premium = document.getElementById("premiumShipping");
+  let express = document.getElementById("expressShipping");
+  let standard = document.getElementById("standardShipping");
+  premium.addEventListener("click", function () {
+    shippingValue = 15;
+    document.getElementById("Total").innerHTML =
+      sTotal + (sTotal * shippingValue) / 100 + "$"
+  });
+  express.addEventListener("click", function () {
+    shippingValue = 7;
+    document.getElementById("Total").innerHTML =
+      sTotal + (sTotal * shippingValue) / 100 + "$"
+  });
+  standard.addEventListener("click", function () {
+    shippingValue = 5;
+    document.getElementById("Total").innerHTML =
+      sTotal + (sTotal * shippingValue) / 100 + "$"
+  });
+
   add.addEventListener("click", async function (e) {
-    console.log(quantity);
     quantity++;
-    console.log(quantity);
     newQuantity.innerHTML = quantity;
-    item.innerHTML = quantity + " item"
+    item.innerHTML = quantity + " item";
     subTotal.innerHTML = quantity * unitCost + "$";
+    sTotal = quantity * unitCost;
+    document.getElementById("Total").innerHTML =
+      sTotal + (sTotal * shippingValue) / 100 + "$";
   });
   sustract.addEventListener("click", async function (e) {
     if (quantity == 0) {
       quantity = 1;
     }
     quantity--;
-    console.log(quantity);
     newQuantity.innerHTML = quantity;
-    item.innerHTML = quantity + " item"
+    item.innerHTML = quantity + " item";
     subTotal.innerHTML = quantity * unitCost + "$";
+    sTotal = quantity * unitCost;
+    document.getElementById("Total").innerHTML =
+      sTotal + (sTotal * shippingValue) / 100 + "$";
   });
+}
+
+function showTotalShipping(quantity, unitCost) {
+  let subTotalforTotal = document.getElementById("subTotal");
 }
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
@@ -77,3 +116,18 @@ document.addEventListener("DOMContentLoaded", async function (e) {
   const cart = (await getJSONData(CART_INFO_URL)).data;
   showCart(cart);
 });
+
+// <div class="total-amount" id="Total">${(article.unitCost * quantity)+((article.unitCost * quantity)/100)}$</div>
+
+//probar con 3 adeventlisteners para los radios
+
+// document.querySelectorAll('input[name="radioShipping"]').forEach((elem) => {
+//   elem.addEventListener("click", function (event) {
+//     var shippingCost = event.target.value;
+//     document.getElementById("save").innerText = shippingCost;
+//     console.log(Number(document.getElementById("save").textContent));
+//     var save = Number(document.getElementById("save").textContent);
+//     document.getElementById("Total").innerHTML = sTotal+(sTotal*save)/100 + "$";
+//     console.log(save);
+//   });
+// });
